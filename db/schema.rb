e23c48d9c2dd10c27c9cda76b64dcfdede0e8c9a
Ayaz <kaxa_sergo90@mail.ru>
2023-12-17 16:13:45 +0300

    rollback migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_17_121321) do
-  create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.integer "post_id", null: false
-    t.integer "parent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["parent_id"], name: "index_comments_on_parent_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_12_02_130023) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -44,7 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_17_121321) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "comments", "comments", column: "parent_id"
-  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
