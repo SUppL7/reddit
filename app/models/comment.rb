7753e1cfa50d7d9ec9599633ba9c71eb1fc3d895
Ayaz <kaxa_sergo90@mail.ru>
2024-01-05 19:06:36 +0300

    new fich

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
    belongs_to :post
    belongs_to :parent, class_name: 'Comment', optional: true
    belongs_to :user
+   validates :body, presence: true
 
    has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
    def can_be_deleted_by?(current_user)
